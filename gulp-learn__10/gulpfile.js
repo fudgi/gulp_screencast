a3fe860cbda146bf8023a82d7e1259321afd64f0
Andrey Seldyukov <a.seldukov@onpoint.ru>
2019-11-24 17:47:37 +0300

    gulp-learn__10 added

---
+++ gulp-learn__10/gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require("gulp");
+const eslint = require("gulp-eslint");
+const debug = require("gulp-debug");
+const through2 = require("through2").obj;
+const fs = require("fs");
+const gulpIf = require("gulp-if");
+const combiner = require("stream-combiner2").obj;
+
+gulp.task("lint", function() {
+  let eslinResults = {};
+
+  let cacheFilePath = process.cwd() + "/tmp/lintCache.json";
+
+  try {
+    eslinResults = JSON.parse(fs.readFileSync(cacheFilePath));
+  } catch (e) {}
+
+  return gulp
+    .src("frontend/**/*.js", { read: false })
+    .pipe(debug({ title: "src" }))
+    .pipe(
+      gulpIf(
+        function(file) {
+          eslinResults[file.path] &&
+            eslinResults[file.path].mtime == file.stat.mtime.toJSON();
+        },
+        through2(function(file, enc, callback) {
+          file.eslint = eslinResults[file.path].eslint;
+          callback(null, file);
+        }),
+        combiner(
+          through2(function(file, enc, callback) {
+            file.contents = fs.readFileSync(file.path);
+            callback(null, file);
+          }),
+          eslint(),
+          through2(function(file, enc, callback) {
+            eslinResults[file.path] = {
+              eslint: file.eslint,
+              mtime: file.stat.mtime
+            };
+            callback(null, file);
+          })
+        )
+      )
+    )
+    .pipe(eslint.format())
+    .on("end", function() {
+      fs.writeFileSync(cacheFilePath, JSON.stringify(eslinResults));
+    });
+});

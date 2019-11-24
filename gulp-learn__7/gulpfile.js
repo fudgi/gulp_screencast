299e18eed9d4ae59a6abc5bca32eef797d853961
Andrey Seldyukov <a.seldukov@onpoint.ru>
2019-11-24 16:03:40 +0300

    8 lesson added in gulp-learn__7

--- gulp-learn__7/gulpfile.js
+++ gulp-learn__7/gulpfile.js
@@ -8,26 +8,51 @@ const sourcemaps = require('gulp-sourcemaps')
 const gulpIf = require('gulp-if')
 const del = require('del')
 const browserSync = require('browser-sync').create()
+const notify  = require('gulp-notify')
+const plumber = require('gulp-plumber')
+const multipipe = require('multipipe')
 
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 gulp.task('styles', () => {
   return gulp.src('frontend/styles/**/main.styl')
+  .pipe(plumber({
+    errorHangler: notify.onError(function(err) {
+        return{
+          title: 'Styles',
+          message: err.message
+        }
+  })}))
   .pipe(gulpIf(isDevelopment, sourcemaps.init()))
   .pipe(debug({title: 'src'}))
   .pipe(stylus())
+  //---------------------------------------------
+  // .on('error', function(err) {
+  //   console.log(err.message)
+  //   this.end()
+  // })
+  //---------------------------------------------
+  // .on('error', notify.onError(function(err) {
+  //   return{
+  //     title: 'Styles',
+  //     message: err.message
+  //   }
+  // }))
   .pipe(concat('main.css'))
   .pipe(gulpIf(isDevelopment, sourcemaps.write()))
   .pipe(gulp.dest('public'))
 })
 
-gulp.task('styles:env', () => {
-  let pipeline = gulp.src('frontend/styles/main.styl');
-  if(isDevelopment) pipeline = pipeline.pipe(sourcemaps.init())
-  pipeline = pipeline.pipe(stylus())
-  if(isDevelopment) pipeline = pipeline.pipe(sourcemaps.write())
-  return pipeline.pipe(gulp.dest('public'))
-})
+gulp.task('styles:multipipe', () => 
+  multipipe(
+    gulpIf(isDevelopment, sourcemaps.init()),
+    stylus(),
+    concat('main.css'),
+    gulpIf(isDevelopment, sourcemaps.write()),
+    gulp.dest('public'),
+  ).on('error', notify.onError)
+)
+
 
 gulp.task('clean', function(){
   return del('public')
